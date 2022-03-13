b125f96fdbec94b50f68e4287ba376489f340f60
felipe_a_gonzalez <gfelipearmando@gmail.com>
2022-03-13 15:50:18 -0600

    Se agrega controllador para la lectura del archivo

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -1,2 +1,14 @@
 class ReadsController < ApplicationController
+    def new 
+        Read.new
+    end
+    def create
+        @read = Read.create(file: params[:file])
+        redirect_to root_path
+    end
+    def index
+        @buyers = Buyer.all
+        @items = Item.all
+        @sellers = Seller.all
+    end
 end

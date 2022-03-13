9c80a7d36408dc3b85be91947615cc54004cc81c
felipe_a_gonzalez <gfelipearmando@gmail.com>
2022-03-13 15:48:59 -0600

    Se agregan las rutas que se usaran por el momento

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "reads#index"
+  get "reads/new", to: "reads#new"
+  post "reads/new", to: "reads#create"
 end
